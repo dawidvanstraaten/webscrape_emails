13b8d6a25045cb0c7ac4d16b72f63809f756d9dc
Dawid  van Straaten <dawidvanstraaten@MacBook-Pro.lan>
2020-11-22 17:57:02 +0200

    Changed the functions a bit. The flow is now that you can get any website. The script looks at the status code returned by the request and then depending on that allows you to continue. If status code is not 200, the website doesn't exist. To save making multiple calls the call falls outside of the functions. To get all the navigation links I use beautiful soup and the select statement to search for all li > a elements which normally are navigation links. I then go through them and remove None, and #.

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ import re
 from bs4 import BeautifulSoup
 
 
-def scrape_website(website):
-    res = requests.get(website)
-
-    if not website_exists(res):
-        print('Website doesn\'t exist.')
-        return
-
+def navigation_links(res):
+    nav_lst = []
     soup = BeautifulSoup(res.text, 'html.parser')
 
     # Navigation elements are typically anchor tags in a list item
     page_links = soup.select('li > a')
     for item in page_links:
-        print(item.get('href', None))
+        if item.get('href', None) == None:
+            continue
+        elif item.get('href', None).strip() != '#':
+            nav_lst.append(item.get('href', None))
+
+    return nav_lst
 
 
 def website_exists(res):
+    res = requests.get(url)
     if res.status_code == 200:
         return True
     else:
         return False
 
-scrape_website('https://davidbombal.com')
+
+while True:
+    url = input('Please enter the url of the website you want to scrape: ')
+    res = requests.get(url)
+    if website_exists(res):
+        break
+    else:
+        print('The url you entered does not exist.')
+
+print(navigation_links(res))
